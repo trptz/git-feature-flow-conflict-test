8e11e8585ad8e38559ae2978cbafc689f65de59e
kentaro_matsutani <kentaro_matsutani@bita.jp>
2021-05-26 19:18:28 +0900

    add first code

---
+++ index.js
@@ -0,0 +1,15 @@
+function hoge() {
+    const hello = 'world';
+    const world = 'hello';
+}
+
+function hello() {
+    const apple = 'りんご';
+}
+
+function world() {
+    setTimeout(() => {
+        // just a moment please
+    }, 1000)
+}
+
